602388859405a64c48381b431c69c524c0d3a8ad
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-19 16:01:05 -0800

    working 1st test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,17 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+  it('can sign up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'eve@evie.com', password: 'Puppies' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'eve@evie.com',
+          __v: 0 
+        });
+      });
   });
 });
